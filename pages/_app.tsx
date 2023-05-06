9b7b6c57efc55d404030e096b99c806abd8784df
Shubham Saxena <91315996+shubham-saxena-pdgt@users.noreply.github.com>
2023-05-06 13:09:20 +0530

    add: font family and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import '@/styles/globals.css';
@@ -6,6 +7,9 @@ import '@/styles/globals.css';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>News Byte</title>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
